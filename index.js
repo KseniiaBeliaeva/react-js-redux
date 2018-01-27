467659581ae932bfb063889deb33bb1e3fdbaeab
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-01-27 11:53:59 +1100

    Template strings exercise

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const integers = [1, 2, 3]; //map
+// const integers = [1, 2, 3]; //map
 // const updatedIntegers = integers.map(function (number) { //number - is a current number in an array;
 //     return number += 1;
 // });
 // const updatedIntegers = integers.map(() => 'bob'); //number - is a current number in an array; => -is a fat array
 // console.log(updatedIntegers);
 
-const ages = [23, 42, 6, 18];
-const adults = ages.filter((age) => age > 20); //returns true or false;
-console.log(adults);
+// const ages = [23, 42, 6, 18];
+// const adults = ages.filter((age) => age > 20); //returns true or false;
+// console.log(adults);
+
+//const name = 'Bill';
+//const age = 35;
+
+// const sentence = 'My name is " + name + ", and I am " + age + " years old";
+// console.log(sentence);
+// FOR TEMPLATE strings we need to use `back-tick` --> ``
+
+//const sentence = `My name is ${name} and I am ${age * 2} years old`;
+//console.log(sentence);
+//age *2 - (age times two)
